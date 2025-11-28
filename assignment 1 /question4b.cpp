3772189dbb5f000a36758fd8a031e940424e43a1
ShlokShukla <83460492+ShlokShukla@users.noreply.github.com>
2025-11-28 17:49:21 +0530

    question4b.cpp

---
+++ assignment 1 /question4b.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a[10][10], b[10][10], c[10][10];
+    int r1, c1, r2, c2;
+    cin >> r1 >> c1;
+    cin >> r2 >> c2;
+
+    if (c1 != r2) 
+    {
+        cout << "matrix multiplication not possible!";
+        return 0;
+    }
+
+    cout << "Enter elements of Matrix A:\n";
+    for (int i = 0; i < r1; i++)
+        for (int j = 0; j < c1; j++)
+            cin >> a[i][j];
+    cout << "Enter elements of Matrix B:\n";
+    for (int i = 0; i < r2; i++)
+        for (int j = 0; j < c2; j++)
+            cin >> b[i][j];
+    for (int i = 0; i < r1; i++) 
+    {
+        for (int j = 0; j < c2; j++) 
+        {
+            c[i][j] = 0;
+            for (int k = 0; k < c1; k++)
+                c[i][j] += a[i][k] * b[k][j];
+        }
+    }
+    cout << "Resultant Matrix C:\n";
+    for (int i = 0; i < r1; i++) 
+    {
+        for (int j = 0; j < c2; j++)
+            cout << c[i][j] << " ";
+        cout << endl;
+    }
+    return 0;
+}
