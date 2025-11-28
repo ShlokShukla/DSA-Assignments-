1311a59f06bdf7cae78ae77345377f325e66362f
ShlokShukla <83460492+ShlokShukla@users.noreply.github.com>
2025-11-28 17:49:56 +0530

    question4c.cpp

---
+++ assignment 1 /question4c.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a[10][10], t[10][10];
+    int r, c;
+    cin >> r >> c;
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            cin >> a[i][j];
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            t[j][i] = a[i][j];
+
+    cout << "Transpose of the matrix:\n";
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < r; j++)
+            cout << t[i][j] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
